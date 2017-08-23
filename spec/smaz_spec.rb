fc6d30adb6db4086cdcfe565a7916cd5b421c6fc
Nick Clifford <nick@nickclifford.me>
2017-08-22 20:20:24 -0500

    get rid of old specs (TODO: write new ones)

--- spec/smaz_spec.rb
+++ spec/smaz_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe Ohm::Smaz do
   let(:str) {'unit testing'}
 
   describe '#compress' do
-    it 'compresses a string with Smaz and converts it to base 220' do
-      expect(Ohm::Smaz.compress(str)).to eq("2c\u00E4\u2591\u00EF\u2320\u2557")
+    it 'compresses a string with Smaz and converts it to base 255' do
+      expect(Ohm::Smaz.compress(str)).to eq("\u203C\u03A3\u03A6\u207C3\u2039")
     end
   end
 
   describe '#decompress' do
-    it 'obtains a string via base 220 and decompresses it with Smaz' do
-      expect(Ohm::Smaz.decompress("2c\u00E4\u2591\u00EF\u2320\u2557")).to eq(str)
+    it 'obtains a string via base 255 and decompresses it with Smaz' do
+      expect(Ohm::Smaz.decompress("\u203C\u03A3\u03A6\u207C3\u2039")).to eq(str)
     end
   end
 end
